1782295a9ad6d4b6490a0e6bba18708577f9cc2f
Ilan Oksenberg <talcohen@tals-MacBook-Pro.local>
2015-04-20 20:33:42 +0300

    First step

---
+++ client/js/app.js
@@ -0,0 +1,18 @@
+(function() {
+  'use strict';
+
+  window.app = angular.module('Pro', ['ngRoute', 'jsTree.directive']).
+  config(['$routeProvider',
+    function($routeProvider) {
+      $routeProvider.
+      when('/', {
+        templateUrl: '../partials/home.html',
+        controller: 'HomeCtrl'
+      }).
+      otherwise({
+        redirectTo: '/home'
+      });
+    }
+  ]);
+
+}());
